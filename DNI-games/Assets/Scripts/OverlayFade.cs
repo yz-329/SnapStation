dd32e6e901d2ea2c6bb9747563cfce8694c92dc9
jolylin620 <zizlin@student.unimelb.edu.au>
2026-05-10 21:47:27 +1000

    overlay script

---
+++ DNI-games/Assets/Scripts/OverlayFade.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OverlayFade : MonoBehaviour
+{
+    public Image overlayImage;
+
+    public float fadeSpeed = 2f;
+
+    private bool fading = false;
+
+    void Update()
+    {
+        // Press SPACE to trigger fade
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            fading = true;
+        }
+
+        // Smooth fade
+        if (fading)
+        {
+            Color color = overlayImage.color;
+
+            color.a = Mathf.Lerp(color.a, 0.5f, fadeSpeed * Time.deltaTime);
+
+            overlayImage.color = color;
+        }
+    }
+}
