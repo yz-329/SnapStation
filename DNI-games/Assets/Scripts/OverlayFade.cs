66be8d867763ee3e6f9cdf958a9d874006f73f67
jolylin620 <zizlin@student.unimelb.edu.au>
2026-05-10 22:12:11 +1000

    Update OverlayFade.cs

--- DNI-games/Assets/Scripts/OverlayFade.cs
+++ DNI-games/Assets/Scripts/OverlayFade.cs
@@ -7,24 +7,28 @@ public class OverlayFade : MonoBehaviour
 
     public float fadeSpeed = 2f;
 
-    private bool fading = false;
+    private bool isDark = false;
 
     void Update()
     {
-        // Take button input to trigger fade
-        // if (Input.GetKeyDown(KeyCode.Space))
-        // {
-        //     fading = true;
-        // }
+        // Take button input to control the overaly background (I used space here)
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isDark = !isDark;
+        }
+
+        Color color = overlayImage.color;
+
+        // Choose target alpha
+        float targetAlpha = isDark ? 0.5f : 0f;
 
         // Smooth fade
-        if (fading)
-        {
-            Color color = overlayImage.color;
+        color.a = Mathf.Lerp(
+            color.a,
+            targetAlpha,
+            fadeSpeed * Time.deltaTime
+        );
 
-            color.a = Mathf.Lerp(color.a, 0.5f, fadeSpeed * Time.deltaTime);
-
-            overlayImage.color = color;
-        }
+        overlayImage.color = color;
     }
 }
