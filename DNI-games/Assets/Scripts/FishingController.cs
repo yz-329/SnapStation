686fbe72dbbe4a28aadecc402c1508af9cc78e37
jolylin620 <zizlin@student.unimelb.edu.au>
2026-05-13 16:02:52 +1000

    Update FishingController.cs

--- DNI-games/Assets/Scripts/FishingController.cs
+++ DNI-games/Assets/Scripts/FishingController.cs
@@ -3,12 +3,21 @@ using UnityEngine;
 public class FishingController : MonoBehaviour
 {
     public GameObject hook; // assign in Inspector
+    private AudioSource hookAudio;
 
+    void Start()
+    {
+        hookAudio = hook.GetComponent<AudioSource>();
+    }
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             hook.SetActive(true);
+            if (hookAudio != null)
+            {
+                hookAudio.Play();
+            }
         }
     }
 }
