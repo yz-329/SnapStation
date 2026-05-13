7c5f11439bd5ac723e1efd92bdfab464bd5c111f
jolylin620 <zizlin@student.unimelb.edu.au>
2026-05-13 16:52:11 +1000

    soundeffect

--- DNI-games/Assets/Scripts/SeaweedTrigger.cs
+++ DNI-games/Assets/Scripts/SeaweedTrigger.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 public class SeaweedTrigger : MonoBehaviour
 {
     private Animator animator;
+    private AudioSource seaweedAudio;
 
     void Start()
     {
         animator = GetComponent<Animator>();
-
+        seaweedAudio = GetComponent<AudioSource>();
         animator.enabled = false;
+        
     }
 
 // take controller input and enable animation
@@ -17,6 +19,11 @@ public class SeaweedTrigger : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             animator.enabled = true;
+            // Play sound once
+            if (seaweedAudio != null)
+            {
+                seaweedAudio.Play();
+            }
         }
     }
 }
