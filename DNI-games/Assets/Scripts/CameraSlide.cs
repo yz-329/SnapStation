0e370c230a7ca6e3b890b3d4f604e9244ba5a355
jolylin620 <zizlin@student.unimelb.edu.au>
2026-05-16 11:54:06 +1000

    update camera slide, food movement

---
+++ DNI-games/Assets/Scripts/CameraSlide.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using UnityEngine;
+
+public class CameraSlide : MonoBehaviour
+{
+    public Transform[] cameraPositions;
+
+    public float slideSpeed = 2f;
+
+    private bool isMoving = false;
+
+    public void MoveToStep(int stepIndex)
+    {
+        if (!isMoving)
+        {
+            StartCoroutine(SmoothMove(cameraPositions[stepIndex].position));
+        }
+    }
+
+    IEnumerator SmoothMove(Vector3 targetPos)
+    {
+        isMoving = true;
+
+        Vector3 startPos = transform.position;
+
+        float t = 0;
+
+        while (t < 1)
+        {
+            t += Time.deltaTime * slideSpeed;
+
+            float smoothT = Mathf.SmoothStep(0, 1, t);
+
+            transform.position = Vector3.Lerp(startPos, targetPos, smoothT);
+
+            yield return null;
+        }
+
+        transform.position = targetPos;
+
+        isMoving = false;
+    }
+}
--- DNI-games/Assets/Scripts/FoodController.cs
+++ DNI-games/Assets/Scripts/FoodController.cs
@@ -2,15 +2,59 @@ using UnityEngine;
 
 public class FoodController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Transform basketPos;
+    public Transform boardPos;
+
+    public float moveSpeed = 5f;
+
+    private bool isMoving = false;
+
+    private bool hasAppeared = false;
+
+    public CameraSlide cameraSlide;
+
     void Start()
     {
-        
+        gameObject.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Here the controller has input (e.g. read UID), food appears.  
+        if (Input.GetKeyDown(KeyCode.Space) && !hasAppeared)
+        {
+            gameObject.SetActive(true);
+
+            transform.position = basketPos.position;
+
+            hasAppeared = true;
+        }
+
+        // A = move food + move camera -- takes input and move food to the chopping area
+        if (Input.GetKeyDown(KeyCode.A) && hasAppeared)
+        {
+            isMoving = true;
+
+            // Move camera to CamPos2
+            cameraSlide.MoveToStep(1);
+        }
+
+        // Move food
+        if (isMoving)
+        {
+            transform.position = Vector2.MoveTowards(
+                transform.position,
+                boardPos.position,
+                moveSpeed * Time.deltaTime
+            );
+
+            // Stop when arrived
+            if (Vector2.Distance(transform.position, boardPos.position) < 0.05f)
+            {
+                transform.position = boardPos.position;
+
+                isMoving = false;
+            }
+        }
     }
 }
